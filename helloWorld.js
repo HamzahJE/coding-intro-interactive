f647adc4e0945c21e4108ac3fdb32c627f7513e2
Hamzah Jamal Eddin <hamzahjeddin@gmail.com>
2022-06-22 21:02:09 +0300

    Completed

---
+++ helloWorld.js
@@ -0,0 +1,47 @@
+console.log ("Hello, Universe")
+
+const firstName = "Hamzah";
+
+let age = 20;
+
+const isCool=true;
+
+let height;
+
+// console.log(typeof firstName)
+// console.log(typeof age)
+// console.log(typeof isCool)
+// console.log(typeof height)
+
+let x=4;
+let y=10;
+let z=7+9;
+let sum =x+y+z;
+sum= x+y-z;
+console.log(sum)
+let diff
+console.log(diff= (x+y) - (y+z));
+
+let string=  "Hello, Universe";
+let welcomeString = `Hello, Universe ${firstName}`
+
+console.log(welcomeString);
+
+if (age>=21) {
+    console.log(`you can drink ${firstName}`)
+} else {
+    console.log(`you've got some growing up to do ${firstName}`)
+}
+
+let score=40;
+let passing=40;
+
+if (score > passing) {
+    console.log(`you pass ${firstName}`)
+} 
+if (passing===score){
+    console.log("just passed");
+} else {
+console.log(`you failed ${firstName}`)
+}
+
